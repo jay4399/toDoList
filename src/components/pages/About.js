8862632a8bb89db1a4338c41aa85355308fd39e9
Jay Rathod <Jay@Jays-MacBook-Pro.local>
2020-09-22 21:28:58 +0400

    Added for deploy

---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is a To Do List app v1.0 that I created as my first small scale React project to learn the language further.</p>
+        </React.Fragment>
+    )
+}
